f75fe13cbe3fdb2e17666b38bf4ab3106f423629
Daria Aleksandrova <106164543+AleksandrovaDaria@users.noreply.github.com>
2023-03-19 12:28:23 +0100

    Delete HR_module directory

--- HR_module/main.py
+++
@@ -1,9 +0,0 @@
-from model import HRModel
-from view import HRView
-from controller import HRController
-
-if __name__ == '__main__':
-    model = HRModel()
-    view = HRView()
-    controller = HRController(model, view)
-    controller.run()
